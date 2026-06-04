5d70a88527a025e16317ea0ed07808f4b3f987d5
Trần Văn Phúc <vphuc@MacBook-Air.local>
2026-06-04 23:31:31 +0700

    Tích hợp PWA cho ứng dụng

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,16 +11,6 @@ firebase.initializeApp({
   appId: "1:314343662236:web:1d0eb2811400a59c054359"
 });
 
-// [HACK FIX] NGĂN HIỂN THỊ 2 THÔNG BÁO TRÊN iOS
-// iOS 16.4+ tự động hiển thị thông báo gốc. Ta cần chặn Firebase cố tình gọi thêm 1 lần nữa.
-const isIos = /iPad|iPhone|iPod/i.test(navigator.userAgent || '');
-if (isIos) {
-  self.registration.showNotification = () => {
-    console.log('Đã chặn Firebase tự hiển thị thông báo thứ 2 trên iOS');
-    return Promise.resolve();
-  };
-}
-
 const messaging = firebase.messaging();
 
 // Xử lý khi nhận được thông báo lúc ứng dụng đang chạy ngầm hoặc đã đóng
